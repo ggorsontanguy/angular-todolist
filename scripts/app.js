fdefd52c6b1676d049136a0d42d39ed528fa5bab
ggorsontanguy <guillaume@gorson-tanguy.net>
2015-10-22 17:01:06 +0200

    Version naïve du bouton suppression (bug dans certains cas"

--- scripts/app.js
+++ scripts/app.js
@@ -15,8 +15,19 @@ app.controller("todoListCtrl", function() {
 	this.todos = [];
 		
 	this.addTodo = function($event) {
-		if ($event.which === 13) {
-			this.todos.push({id : nbTodo++, name:this.todo.name});			
+		if ($event.which === 13 && this.todo.name!=="") {
+			this.todos.push({id : nbTodo++, name:this.todo.name});
+			this.todo.name = "";
+		}
+	}
+	
+	this.delTodo = function(id) {
+		console.log(id + " del " + this.todos.length )
+		if (this.todos.length > 1) {
+			this.todos.splice(id, 1);
+		} else {
+			this.todos = [];
+			nbTodo = 0;
 		}
 	}
 
