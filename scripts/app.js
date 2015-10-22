f2b2b37e38ed5e64544fb856f1bacd1061264741
ggorsontanguy <guillaume@gorson-tanguy.net>
2015-10-22 15:10:59 +0200

    suppression method indexof inutile

--- scripts/app.js
+++ scripts/app.js
@@ -13,22 +13,10 @@ app.controller("todoListCtrl", function() {
 	}
 	
 	this.todos = [];
-	
-	if(!Array.indexOf){
-		Array.prototype.indexOf = function(obj){
-			for(var i=0; i<this.length; i++){
-				if(this[i]==obj) return i;
-			}
-		return -1;
-		}
-	}
 		
 	this.addTodo = function($event) {
 		if ($event.which === 13) {
-			//if (this.todos.indexOf(this.todo)===-1) {
-				this.todos.push({id : nbTodo++, name:this.todo.name});
-			//	}
-				
+			this.todos.push({id : nbTodo++, name:this.todo.name});			
 		}
 	}
 
