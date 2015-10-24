4204f1cb8eb135edff6f8b197f71a4f9102a4777
ggorsontanguy <guillaume@gorson-tanguy.net>
2015-10-24 03:40:41 +0200

    Ajout de la fonctionalité permettant de cocher une tâche faite.

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,23 @@
 var app = angular.module("todolist", []);
 
 app.controller("exempleCtrl", function($scope) {
-    $scope.name = "World"
+    $scope.name = "World... This a TodoList"
 });
 
 app.controller("todoListCtrl", function() {
 	
-	function ItemTodo(name) {
+	function ItemTodo(name, checked) {
 		this.name = name;
+		this.checked = checked;
 	}
 	
-	this.todo = new ItemTodo("");
+	this.todo = new ItemTodo("", false);
 	
 	this.todos = [];
 		
 	this.addTodo = function($event) {
 		if ($event.which === 13 && this.todo.name!=="") {
-			this.todos.push(new ItemTodo(this.todo.name));
+			this.todos.push(new ItemTodo(this.todo.name, false));
 			this.todo.name = "";
 		}
 	}
